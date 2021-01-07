d80cb2b04ba67b8469550d4b48454ff501f425fb
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-01-07 15:52:28 +0100

    Delete all, rebasing on the Battery Engine

--- CombineInstallers/combineInstallers.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo "Combining x64 installers..."
-
-iexpress /N Combine_Script.sed
--- ReleaseArchiver/createReleaseArchive.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set archive=AutomaticUpdaterRelease.zip
-
-if exist %archive% del %archive%
-
-cd ../x64/Release && call "../../ReleaseArchiver/7z.exe" a -tzip ../../ReleaseArchiver/%archive% *.dll *.exe
