f62c3a9770e0b08516d930186bd4d5cc698e27c7
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-01-09 20:07:59 +0100

    Added icons

--- include/Application.h
+++ include/Application.h
@@ -6,13 +6,22 @@ class Application : public Battery::Engine {
 public:
 	
 	bool Setup() override {
+
+		//Battery::Updater::CheckForGithubUpdate("HerrNamenlos123", "TechnicalSketcher");
+		Battery::NetUtils::DownloadFile("https://github.com/HerrNamenlos123/TechnicalSketcher/releases/download/v0.1-alpha.3/TechnicalSketcher-Setup-x64.msi"
+			, "testFile.bin", "cacert.pem");
+		
+		/*std::cout << "Filesize: " << (int64_t)Battery::NetUtils::GetSizeOfDownloadFile(
+			"https://github.com/HerrNamenlos123/TechnicalSketcher/releases/download/v0.1-alpha.3/TechnicalSketcher-Setup-x64.msi",
+			"cacert.pem", "gister") << std::endl;*/
+
 		return true;
 	}
 
 	bool Update() override {
 		using namespace Battery::Graphics;
 		
-		DrawBackground({ 255, 255, 255 });
+		DrawBackground({ 0, 0, 0 });
 
 		ImGui::Begin("Test");
 		ImGui::End();
